55497e9f7b5dc0ac94deacde8035872e64ab9f24
苏泽 <havonz@suzes-MacBook-Pro.local>
2018-03-08 22:44:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ rm -f _book/README.md.time
 gitbook build . --log=debug
 
 # 3 - Sync (Please configure authorized_keys before this step)
-rsync -avz --progress _book/ root@106.14.93.80::www
+rsync -avz --progress _book/ root@106.14.93.80:/home/www
 
 # 4 - CDN
 # aliyuncli cdn RefreshObjectCaches --ObjectPath https://kb.xxtouch.com/
